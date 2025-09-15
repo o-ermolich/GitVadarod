fbb4730afc33c58504b0ea7b6a8e8b362a2f3605
Olga Ermolich <o.ermolich@gmail.com>
2025-09-15 15:55:41 +0300

    Task 1

--- src/ermolich_homework/task_1/Task1.java
+++ src/ermolich_homework/task_1/Task1.java
@@ -1,4 +1,6 @@
-public class Main {
+package ermolich_homework.task_1;
+
+public class Task1 {
     public static void main(String[] args) {
         System.out.println("Ermolich Olga; 10.09.2025 19:00");
     }
