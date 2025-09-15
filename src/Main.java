20681d685636030cfd4a14025dc6a6888990c356
Olga Ermolich <o.ermolich@gmail.com>
2025-09-15 15:22:39 +0300

    task1.GitProject

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.println("Ermolich Olga; 10.09.2025 19:00");
+    }
+}
