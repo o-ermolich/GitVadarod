9f715919c2c1ecec77ea2ab1040659601b64242a
Olga Ermolich <o.ermolich@gmail.com>
2025-11-29 18:55:53 +0300

    Task 20

---
+++ src/ermolich_homework/task_20/Main.java
@@ -0,0 +1,27 @@
+package ermolich_homework.task_20;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+public class Main {
+    public static void main(String[] args) {
+
+        ExecutorService executorService = Executors.newFixedThreadPool(3);
+
+        for (int i = 0; i < 10; i++) {
+            StringGenerator stringGenerator = new StringGenerator(i + 1);
+            executorService.submit(stringGenerator);
+        }
+
+        executorService.shutdown();
+        try {
+
+            if (!executorService.awaitTermination(60, TimeUnit.SECONDS)) {
+                executorService.shutdownNow();
+            }
+        } catch (InterruptedException e) {
+            executorService.shutdownNow();
+        }
+    }
+}
